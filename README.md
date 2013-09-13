098e76fb3fcb966cc79e30f9ea673c382bb7a64e
RenatoCJN <renatocjn@gmail.com>
2013-09-13 15:02:11 -0300

    Complementando o script de estatisticas; extrai mais valores do xml; calcula media, desvio padrao e intervalo de confianca

--- README.md
+++ README.md
@@ -10,8 +10,14 @@ cp special-wscript ../wscript
 ```
 it will make the waf builder search these folders
 
-###folders
+###Folders
 
 - results - this folder contains the results files of the simulations
 - scripts - this folder contains the scripts that run the simulations and organize the results
 - src - this folder contains the ns3 source code in C
+
+###Python dependencies
+
+*these are used to calculate statistics*
+- numpy
+- scikits.bootstrap
--- scripts/statistics.py
+++ scripts/statistics.py
@@ -2,8 +2,13 @@
 
 from lxml import etree
 from glob import glob
-import os
-import sys
+import os, sys, numpy
+from scikits.bootstrap import ci
+def statistics(vals):
+	vals = numpy.array(vals)
+	return vals.mean(), vals.std(), ci( vals, numpy.average )
+
+clean_result = lambda x: float( filter( lambda x: x.isdigit() or x=='.', x ) )
 
 if os.path.isdir(sys.argv[1]):
 	os.chdir(sys.argv[1])
@@ -13,30 +18,44 @@ else:
 
 _, directories, _ = os.walk('.').next()
 
+txBytes = list()
+rxBytes = list()
+timeFirstTxPacket = list()
+timeFirstRxPacket = list()
+timeLastTxPacket = list()
+timeLastRxPacket = list()
+deliveryRate = list()
+delay = list()
+delaySum = list()
+jitterSum = list()
+lastDelay = list()
+lostPackets = list()
+timesForwarded = list()
+
 for d in directories:
 	os.chdir(d)
-	print 'para', d
 	xmlString = open('FlowMonitorResults.xml', 'r').read()
 	xmlRoot = etree.XML(xmlString)
 	FlowStats = xmlRoot.find('FlowStats')
-
 	flowcount=0
-
 	for flow in FlowStats.iterfind('Flow'):
-		rxBytes = float( filter( lambda x: x.isdigit() or x=='.', flow.get('rxBytes') ) )
-		txBytes = float( filter( lambda x: x.isdigit() or x=='.', flow.get('txBytes') ) )
-		deliveryRate = rxBytes/txBytes
-		timeLastRxPacket = float( filter( lambda x: x.isdigit() or x=='.', flow.get('timeLastRxPacket') ) )
-		timeFirstTxPacket = float( filter( lambda x: x.isdigit() or x=='.', flow.get('timeFirstTxPacket') ) )
-		delay = (timeLastRxPacket - timeFirstTxPacket)/1000000000
-		print '\tPara o flow', flow.get('flowId'), 'foi recuperado delay(s):',delay, 'e taxa de entrega(%):', deliveryRate, '\n'
-	os.chdir('..')
+		flowcount+=1
+		rxBytes.append(clean_result(flow.get('rxBytes')))
+		txBytes.append(clean_result(flow.get('txBytes')))
+		timeLastRxPacket.append(clean_result(flow.get('timeLastRxPacket')))
+		timeFirstTxPacket.append(clean_result(flow.get('timeFirstTxPacket')))
+		delaySum.append(clean_result(flow.get('delaySum')))
+		jitterSum.append(clean_result(flow.get('jitterSum')))
+		lastDelay.append(clean_result(flow.get('lastDelay')))
+		lostPackets.append(clean_result(flow.get('lostPackets')))
+		timesForwarded.append(clean_result(flow.get('timesForwarded')))
+	os.chdir(os.pardir)
 
+deliveryRate = map( lambda x: x[0]/x[1], zip(rxBytes, txBytes))
+delay = map ( lambda x: (x[0]-x[1])*10**(-9), zip(timeLastRxPacket, timeFirstTxPacket))
 
-
-
-
-
-
+print deliveryRate
+print 'delivery statistics:', statistics(deliveryRate)
+print 'delay statistics:', statistics(delay)
 
 
